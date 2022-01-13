c2c9852645098b22854ce81e50bb74da46a987ca
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2022-01-13 12:12:17 -0800

    adding seed data from csv

--- sql/product.pg.sql
+++ sql/product.pg.sql
@@ -1,25 +1,36 @@
-DROP SCHEMA if exists productsSchema;
-DROP SCHEMA if exists productsSchema;
+-- -----------------------------------------------------
+-- Drop and Create database and schema
+-- -----------------------------------------------------
+
+DROP DATABASE IF EXISTS productsdb;
+CREATE DATABASE productsdb;
+\connect productsdb;
+DROP SCHEMA if exists productsschema;
+CREATE SCHEMA productsschema;
+
+-- -----------------------------------------------------
+-- Drop tables
+-- -----------------------------------------------------
+
+DROP TABLE IF EXISTS productsschema.products CASCADE;
+DROP TABLE IF EXISTS productsschema.photos CASCADE;
+DROP TABLE IF EXISTS productsschema.product_related CASCADE;
+DROP TABLE IF EXISTS productsschema.styles CASCADE;
+DROP TABLE IF EXISTS productsschema.skus CASCADE;
+
 
 -- -----------------------------------------------------
 -- Table productsDB.products
 -- -----------------------------------------------------
 
-DROP TABLE IF EXISTS productsDB.products CASCADE;
-DROP TABLE IF EXISTS productsDB.photos CASCADE;
-DROP TABLE IF EXISTS productsDB.product_related CASCADE;
-DROP TABLE IF EXISTS productsDB.styles CASCADE;
-DROP TABLE IF EXISTS productsDB.skus CASCADE;
-
-
-CREATE TABLE productsDB.products (
+CREATE TABLE productsschema.products (
   id SERIAL UNIQUE PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   campus VARCHAR(255) NOT NULL,
   slogan VARCHAR(255) NOT NULL,
   description VARCHAR(255) NOT NULL,
   category VARCHAR(255) NOT NULL,
-  default_price DECIMAL(10,0) NOT NULL,
+  default_price VARCHAR(255) NOT NULL,
   createdAt TIMESTAMP NOT NULL,
   updatedAt TIMESTAMP NOT NULL
 );
@@ -27,7 +38,7 @@ CREATE TABLE productsDB.products (
 -- -----------------------------------------------------
 -- Table photos
 -- -----------------------------------------------------
-CREATE TABLE productsDB.photos (
+CREATE TABLE productsschema.photos (
   id SERIAL PRIMARY KEY,
   product_id INT NULL DEFAULT NULL,
   thumbnail_url VARCHAR(255) NOT NULL,
@@ -36,7 +47,7 @@ CREATE TABLE productsDB.photos (
   updatedAt TIMESTAMP NOT NULL,
   CONSTRAINT photos_fk
     FOREIGN KEY (product_id)
-    REFERENCES productsDB.products(id)
+    REFERENCES productsschema.products(id)
     ON DELETE SET NULL
     ON UPDATE CASCADE
 );
@@ -44,19 +55,19 @@ CREATE TABLE productsDB.photos (
 -- -----------------------------------------------------
 -- Table product_related
 -- -----------------------------------------------------
-CREATE TABLE productsDB.product_related (
+CREATE TABLE productsschema.related (
   product_id INT NOT NULL PRIMARY KEY,
-  product_id_related INT NOT NULL,
+  related_id INT NOT NULL,
   createdAt TIMESTAMP NOT NULL,
   updatedAt TIMESTAMP NOT NULL,
   CONSTRAINT product_related_ibfk_1
-    FOREIGN KEY (product_id_related)
-    REFERENCES productsDB.products (id)
+    FOREIGN KEY (related_id)
+    REFERENCES productsschema.products (id)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT product_related_ibfk_2
     FOREIGN KEY (product_id)
-    REFERENCES productsDB.products (id)
+    REFERENCES productsschema.products (id)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
@@ -64,7 +75,7 @@ CREATE TABLE productsDB.product_related (
 -- -----------------------------------------------------
 -- Table styles
 -- -----------------------------------------------------
-CREATE TABLE productsDB.styles (
+CREATE TABLE productsschema.styles (
   id SERIAL PRIMARY KEY,
   productId INT NULL DEFAULT NULL,
   name VARCHAR(255) NOT NULL,
@@ -72,7 +83,7 @@ CREATE TABLE productsDB.styles (
   updatedAt TIMESTAMP NOT NULL,
   CONSTRAINT styles_ibfk_1
     FOREIGN KEY (productId)
-    REFERENCES productsDB.products (id)
+    REFERENCES productsschema.products (id)
     ON DELETE SET NULL
     ON UPDATE CASCADE
 );
@@ -80,7 +91,7 @@ CREATE TABLE productsDB.styles (
 -- -----------------------------------------------------
 -- Table skus
 -- -----------------------------------------------------
-CREATE TABLE productsDB.skus (
+CREATE TABLE productsschema.skus (
   id SERIAL PRIMARY KEY ,
   styleId INT NULL DEFAULT NULL,
   size VARCHAR(255) NOT NULL,
@@ -88,7 +99,11 @@ CREATE TABLE productsDB.skus (
   updatedAt TIMESTAMP NOT NULL,
   CONSTRAINT skus_ibfk_1
     FOREIGN KEY (styleId)
-    REFERENCES productsDB.styles (id)
+    REFERENCES productsschema.styles (id)
     ON DELETE SET NULL
     ON UPDATE CASCADE
 );
+
+-- -----------------------------------------------------
+-- Populate db schema tables
+-- -----------------------------------------------------
---
+++ sql/seed.sql
@@ -0,0 +1,6 @@
+\connect productsdb;
+\COPY productsschema.products FROM '/Users/nanda/Projects/galvanizeNov/perceptive-peanut/products/csv/product.csv' DELIMITER ',' CSV HEADER;
+\COPY productsschema.photos FROM '/Users/nanda/Projects/galvanizeNov/perceptive-peanut/products/csv/photos.csv' DELIMITER ',' CSV HEADER;
+\COPY productsschema.related FROM '/Users/nanda/Projects/galvanizeNov/perceptive-peanut/products/csv/related.csv' DELIMITER ',' CSV HEADER;
+\COPY productsschema.styles FROM '/Users/nanda/Projects/galvanizeNov/perceptive-peanut/products/csv/styles.csv' DELIMITER ',' CSV HEADER;
+\COPY productsschema.skus FROM '/Users/nanda/Projects/galvanizeNov/perceptive-peanut/products/csv/skus.csv' DELIMITER ',' CSV HEADER;
