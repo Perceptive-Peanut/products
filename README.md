7453460d2066f9cc37cfab83746177e1862ca170
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2022-01-13 16:30:51 -0800

    updates

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Products
+
+## Create DB and Schema
+From the product root directory run:
+```bash
+psql postgres  < sql/product.pg.sql
+```
+
+## Add seed data to database
+```bash
+psql postgres  < sql/seed.sql
+```
+
+## Create DB & Add seed
+```bash
+psql postgres  < sql/product.pg.sql && psql postgres  < sql/seed.sql
+```
+
+## Get CSV columns
+```bash
+head -1 csv/product.csv # id,name,slogan,description,category,default_price
+head -1 csv/photos.csv # id,styleId,url,thumbnail_url
+head -1 csv/related.csv # id,current_product_id,related_product_id
+head -1 csv/skus.csv # id,styleId,size,quantity
+head -1 csv/styles.csv # id,productId,name,sale_price,original_price,default_style
+```
+
--- sql/product.pg.sql
+++ sql/product.pg.sql
@@ -22,86 +22,74 @@ DROP TABLE IF EXISTS productsschema.skus CASCADE;
 -- -----------------------------------------------------
 -- Table productsDB.products
 -- -----------------------------------------------------
-
+-- id,name,slogan,description,category,default_price
 CREATE TABLE productsschema.products (
   id SERIAL UNIQUE PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
-  campus VARCHAR(255) NOT NULL,
   slogan VARCHAR(255) NOT NULL,
-  description VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
   category VARCHAR(255) NOT NULL,
-  default_price VARCHAR(255) NOT NULL,
-  createdAt TIMESTAMP NOT NULL,
-  updatedAt TIMESTAMP NOT NULL
-);
-
--- -----------------------------------------------------
--- Table photos
--- -----------------------------------------------------
-CREATE TABLE productsschema.photos (
-  id SERIAL PRIMARY KEY,
-  product_id INT NULL DEFAULT NULL,
-  thumbnail_url VARCHAR(255) NOT NULL,
-  url VARCHAR(255) NOT NULL,
-  createdAt TIMESTAMP NOT NULL,
-  updatedAt TIMESTAMP NOT NULL,
-  CONSTRAINT photos_fk
-    FOREIGN KEY (product_id)
-    REFERENCES productsschema.products(id)
-    ON DELETE SET NULL
-    ON UPDATE CASCADE
-);
-
--- -----------------------------------------------------
--- Table product_related
--- -----------------------------------------------------
-CREATE TABLE productsschema.related (
-  product_id INT NOT NULL PRIMARY KEY,
-  related_id INT NOT NULL,
-  createdAt TIMESTAMP NOT NULL,
-  updatedAt TIMESTAMP NOT NULL,
-  CONSTRAINT product_related_ibfk_1
-    FOREIGN KEY (related_id)
-    REFERENCES productsschema.products (id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT product_related_ibfk_2
-    FOREIGN KEY (product_id)
-    REFERENCES productsschema.products (id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE
+  default_price INT NOT NULL
 );
 
 -- -----------------------------------------------------
 -- Table styles
 -- -----------------------------------------------------
+-- id,productId,name,sale_price,original_price,default_style
 CREATE TABLE productsschema.styles (
   id SERIAL PRIMARY KEY,
   productId INT NULL DEFAULT NULL,
   name VARCHAR(255) NOT NULL,
-  createdAt TIMESTAMP NOT NULL,
-  updatedAt TIMESTAMP NOT NULL,
+  sale_price INT,
+  original_price INT NOT NULL,
+  default_style INT NOT NULL,
   CONSTRAINT styles_ibfk_1
     FOREIGN KEY (productId)
     REFERENCES productsschema.products (id)
-    ON DELETE SET NULL
-    ON UPDATE CASCADE
+);
+
+-- -----------------------------------------------------
+-- Table photos
+-- -----------------------------------------------------
+-- id,styleId,url,thumbnail_url
+CREATE TABLE productsschema.photos (
+  id SERIAL PRIMARY KEY,
+  styleId INT NULL DEFAULT NULL,
+  thumbnail_url VARCHAR(255) NOT NULL,
+  url TEXT NOT NULL,
+  CONSTRAINT photos_fk
+    FOREIGN KEY (styleId)
+    REFERENCES productsschema.styles(id)
+);
+
+-- -----------------------------------------------------
+-- Table product_related
+-- -----------------------------------------------------
+-- id,current_product_id,related_product_id
+CREATE TABLE productsschema.related (
+  id INT NOT NULL PRIMARY KEY,
+  current_product_id INT NOT NULL,
+  related_product_id INT NOT NULL,
+  CONSTRAINT product_related_ibfk_1
+    FOREIGN KEY (current_product_id)
+    REFERENCES productsschema.products (id),
+  CONSTRAINT product_related_ibfk_2
+    FOREIGN KEY (related_product_id)
+    REFERENCES productsschema.products (id)
 );
 
 -- -----------------------------------------------------
 -- Table skus
 -- -----------------------------------------------------
+-- id,styleId,size,quantity
 CREATE TABLE productsschema.skus (
   id SERIAL PRIMARY KEY ,
   styleId INT NULL DEFAULT NULL,
   size VARCHAR(255) NOT NULL,
-  createdAt TIMESTAMP NOT NULL,
-  updatedAt TIMESTAMP NOT NULL,
+  quantity INT NOT NULL,
   CONSTRAINT skus_ibfk_1
     FOREIGN KEY (styleId)
     REFERENCES productsschema.styles (id)
-    ON DELETE SET NULL
-    ON UPDATE CASCADE
 );
 
 -- -----------------------------------------------------
