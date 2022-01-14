9c4cea28fa1c6ff3bac1bea0e62efd7c69444934
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2022-01-14 01:08:45 -0800

    Server setup

---
+++ src/db/postgres.js
@@ -0,0 +1,19 @@
+const { Client } = require('pg');
+
+const { PGUSER, PGPASSWORD, PGHOST, PGPORT, PGDATABASE } = process.env;
+
+console.log('PGDATABASE', PGDATABASE)
+
+const db = new Client({
+  host: PGHOST,
+  port: PGPORT,
+  user: PGUSER,
+  password: PGPASSWORD,
+  database: PGDATABASE,
+});
+
+db.connect()
+  .then(() => console.log('connected'))
+  .catch(err => console.error('connection error', err.stack))
+
+module.exports = db;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const products = require('./routes/products');
+
+
+// middlewares
+app.use(express.json());
+
+// routes
+app.use('/products', products);
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`App is running on port ${process.env.SERVER_PORT}`);
+});
